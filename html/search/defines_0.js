61fd8d5f01fd224ff2db8d5489f1b4bbfb594acd
Travis CI <travis@travis-ci.org>
2019-06-06 18:27:57 +0000

    Deploy code docs to GitHub Pages Travis build: 16
    Commit: db22627689ab54a27c1fb0f3d9e8b3531dd81624

--- html/search/defines_0.js
+++ html/search/defines_0.js
@@ -1,4 +1,6 @@
 var searchData=
 [
-  ['mpr121_5fi2caddr_5fdefault',['MPR121_I2CADDR_DEFAULT',['../_adafruit___m_p_r121_8h.html#aa4a9474e42d9c498f600c9ad8781f714',1,'Adafruit_MPR121.h']]]
+  ['mpr121_5fi2caddr_5fdefault',['MPR121_I2CADDR_DEFAULT',['../_adafruit___m_p_r121_8h.html#aa4a9474e42d9c498f600c9ad8781f714',1,'Adafruit_MPR121.h']]],
+  ['mpr121_5frelease_5fthreshold_5fdefault',['MPR121_RELEASE_THRESHOLD_DEFAULT',['../_adafruit___m_p_r121_8h.html#af51b589d3c7f29cc43fff6f96715a0e9',1,'Adafruit_MPR121.h']]],
+  ['mpr121_5ftouch_5fthreshold_5fdefault',['MPR121_TOUCH_THRESHOLD_DEFAULT',['../_adafruit___m_p_r121_8h.html#a4b3fd951704a4d69529cec6c8996a560',1,'Adafruit_MPR121.h']]]
 ];
