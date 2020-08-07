aad05b35021b79ba216c94961349afad90d8f31d
Mathistis <43539043+Mathistis@users.noreply.github.com>
2020-08-07 10:55:52 +0200

    Update Adafruit_MPR121.cpp
     No need to modify the register ECR in this function because the stop is already done in function "writeRegister()".
    
    The following line : writeRegister(MPR121_ECR, 0x8F);
    cause an issue because is restart the MPR121 with a constant value : 0x8F
    if the user modified previously, and then set the treshold, user setting are lost.
    
    mathis.raemy@gmail.com for more informations

--- Adafruit_MPR121.cpp
+++ Adafruit_MPR121.cpp
@@ -140,15 +140,11 @@ void Adafruit_MPR121::setThreshholds(uint8_t touch, uint8_t release) {
  *              the release threshold from 0 to 255.
  */
 void Adafruit_MPR121::setThresholds(uint8_t touch, uint8_t release) {
-  // first stop sensor to make changes
-  writeRegister(MPR121_ECR, 0x00);
   // set all thresholds (the same)
   for (uint8_t i = 0; i < 12; i++) {
     writeRegister(MPR121_TOUCHTH_0 + 2 * i, touch);
     writeRegister(MPR121_RELEASETH_0 + 2 * i, release);
   }
-  // turn the sensor on again
-  writeRegister(MPR121_ECR, 0x8F);
 }
 
 /*!
