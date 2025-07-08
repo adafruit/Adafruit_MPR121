b6cbd6238a5681505721dd70c42fcf7db356fec4
Doxygen CI <ci-arduino@invalid>
2025-07-08 14:36:08 +0000

    Deploy docs to GitHub Pages from commit 94f3a35881
    Commit: 94f3a35881dc150e3ee312485dea216d9c46259e
    GitHub Actions run: 16146176156

--- html/search/all_1.js
+++ html/search/all_1.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['baselinedata',['baselineData',['../class_adafruit___m_p_r121.html#a663d5e60aeb04b06697b2c40a33b40f0',1,'Adafruit_MPR121']]],
-  ['begin',['begin',['../class_adafruit___m_p_r121.html#ac62a47c152877a4fd9ee08dd6fbfc8e6',1,'Adafruit_MPR121']]]
+  ['begin',['begin',['../class_adafruit___m_p_r121.html#a0f961c9913525f3d8b214127cbdb3339',1,'Adafruit_MPR121']]]
 ];
--- html/search/functions_4.js
+++ html/search/functions_4.js
@@ -1,5 +1,6 @@
 var searchData=
 [
+  ['setautoconfig',['setAutoconfig',['../class_adafruit___m_p_r121.html#a66adfa0474632af6bcd0dbb0d5d94801',1,'Adafruit_MPR121']]],
   ['setthreshholds',['setThreshholds',['../class_adafruit___m_p_r121.html#a307f1c580221584c75eb0c5da34982e0',1,'Adafruit_MPR121']]],
   ['setthresholds',['setThresholds',['../class_adafruit___m_p_r121.html#a8588eabc494059b6fe5231b8dd2be416',1,'Adafruit_MPR121']]]
 ];
